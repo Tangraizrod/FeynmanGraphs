8e4f360bc512b4e7af12f22fb1404f4d0a01bc21
Tangraizrod <bogiswag12321@gmail.com>
2023-06-24 10:58:33 +0100

    Finalised NP-complete problem

--- functions.py
+++ functions.py
@@ -16,6 +16,8 @@ import numpy as np
 import lime.lime_tabular
 from sklearn.metrics import accuracy_score
 import seaborn as sns
+from re import *
+from itertools import combinations
 
 
 
@@ -130,6 +132,8 @@ def print_graph_info(grakel_graph):
     num_edges = len(edges)
 
     print("Number of edges: ", int(num_edges/2))
+    print(edges)
+    print(node_labels)
 
 # Load the JSON file
 def convert_dict_to_grakel_graphs(data_dict):
@@ -163,11 +167,22 @@ def parse_input(grakel_graphs, h):
     features = gk.parse_input(grakel_graphs)
     return features
 
+# def parse_input_vh(grakel_graphs):
+#     gk = VertexHistogram()
+#     gk.fit_transform(grakel_graphs)
+    
+#     return 
+
 def representation(grakel_graphs, h):
     # Initialize the WeisfeilerLehmanOptimalAssignment kernel
     gk = WeisfeilerLehmanOptimalAssignment(n_iter = h, normalize=True)
 
     # Fit and transform the kernel on the input graph list
+    gk.fit_transform(grakel_graphs)
+    return gk._inv_labels
+
+def representation_vh(grakel_graphs):
+    gk = VertexHistogram()
     gk.fit_transform(grakel_graphs)
     return gk._inv_labels
 
@@ -222,6 +237,38 @@ def lime_explainer(grakel_graphs, class_list, index):
     classes = np.array(class_list)
     my_list = parse_input(grakel_graphs, 1)
     my_list = [lst[22:] for lst in my_list]
+    my_list = np.array(my_list)
+    rf = RandomForestClassifier(n_estimators=150, random_state=42)
+    rf.fit(my_list, classes)
+
+    explainer = lime.lime_tabular.LimeTabularExplainer(my_list, class_names = feature_names_list, discretize_continuous=True, random_state=42)
+    # An instance to justify the model's selection
+    i = index
+    instance = my_list[i]
+
+    # Generate an explanation
+    exp = explainer.explain_instance(instance, rf.predict_proba, num_features=20, top_labels=3)
+
+    # Visualize the explanation for each class
+    for i in range(3):
+        print('Explanation for class %s:' % i)
+        exp.show_in_notebook(show_table=True, labels=[i])
+    # Get the explanations as a list
+    # Define the classes
+    classes_list = [0, 1, 2]  # Update this to match your actual classes
+    feature_weights_output = []
+    # For each class, get and print the explanations
+    for class_id in classes_list:
+        explanation_list = exp.as_list(label=class_id)
+        for feature_weight in explanation_list:
+            feature_weights_output.append([feature_weight])
+    return feature_weights_output
+
+def lime_explainer_vh(grakel_graphs, class_list, index):
+    feature_names_list = ["strong", "weak", "electromagnetic"]
+    # Create numpy array from the classes list
+    classes = np.array(class_list)
+    my_list = parse_input_vh(grakel_graphs, 1)
     my_list = np.array(my_list)
     rf = RandomForestClassifier(n_estimators=150, random_state=42)
     rf.fit(my_list, classes)
@@ -305,6 +352,141 @@ def plot_confusion_matrix(grakel_graphs, class_list):
     plt.xlabel('Predicted')
     plt.ylabel('True')
     plt.show()
+
+def grakel_to_networkx(G):
+    g_nx = nx.Graph()
+
+    for u, v, data in G.edges(data=True):
+        g_nx.add_edge(u, v, **data)
+
+    for n, data in G.nodes(data=True):
+        g_nx.nodes[n].update(data)
+
+    return g_nx
+
+def find_substructure(graph, labels):
+    subgraph_nodes = []
+    
+    for node in graph.nodes(data=True):
+        if node[1]['label'] in labels:
+            subgraph_nodes.append(node[0])
+            labels.remove(node[1]['label'])
+
+    # Find the edges that are part of the substructure
+    subgraph_edges = [edge for edge in graph.edges() if edge[0] in subgraph_nodes and edge[1] in subgraph_nodes]
+    
+    return subgraph_nodes, subgraph_edges
+
+def visualize_substructure(graph, subgraph_nodes, subgraph_edges):
+    pos = nx.spring_layout(graph)  # position layout
+    plt.figure(figsize=(8, 6))
+
+    # draw all nodes and edges in the graph in light color
+    nx.draw_networkx(graph, pos, node_color='blue', edge_color='black')
+    
+    # draw the nodes and edges of the subgraph in dark color
+    nx.draw_networkx_nodes(graph, pos, nodelist=subgraph_nodes, node_color='red')
+    nx.draw_networkx_edges(graph, pos, edgelist=subgraph_edges, edge_color='red')
+    nx.draw_networkx_labels(graph, pos)
+    plt.show()
+
+def find_substructure(graph, labels):
+    # Define substructure finding for small number of labels
+    def modified_method(labels):
+        subgraph_nodes = set()
+        subgraph_edges = set()
+
+        # Check all edges in the graph
+        for u, v, data in graph.edges(data=True):
+            edge_labels = [graph.nodes[u]['label'], graph.nodes[v]['label']]
+            if all(any(label.strip("_") in edge_label for edge_label in edge_labels) for label in labels):
+                subgraph_nodes.update([u, v])
+                subgraph_edges.add((u, v))
+        return subgraph_nodes, subgraph_edges
+
+    # If number of labels is small, use modified method
+    if len(labels) <= 3:
+        return modified_method(labels)
+
+    # If number of labels is greater than 3, use exhaustive search
+    else:
+        priority_labels = ['Z', 'W', 'g', 'gamma']
+
+        # Sort labels according to the priority list
+        labels.sort(key=lambda x: priority_labels.index(x) if x in priority_labels else len(priority_labels))
+
+        all_nodes = [node for node in graph.nodes(data=True)]
+        for nodes_comb in combinations(all_nodes, 4):
+            subgraph_nodes = [node[0] for node in nodes_comb]
+            subgraph = graph.subgraph(subgraph_nodes)
+            node_labels = [data['label'] for _, data in subgraph.nodes(data=True)]
+
+            if set(labels) == set(node_labels) and nx.is_connected(subgraph):
+                subgraph_edges = [edge for edge in graph.edges() if edge[0] in subgraph_nodes and edge[1] in subgraph_nodes]
+                return subgraph_nodes, subgraph_edges
+
+        # If no suitable subgraph found
+        return set(), set()
+
+
+
+
+
+
+def visualize_substructure(graph, subgraph_nodes, subgraph_edges):
+    pos = nx.spring_layout(graph)  # position layout
+    plt.figure(figsize=(8, 6))
+
+    # draw all nodes and edges in the graph in light color
+    nx.draw_networkx(graph, pos, node_color='blue', edge_color='black')
+    
+    # draw the nodes and edges of the subgraph in dark color
+    nx.draw_networkx_nodes(graph, pos, nodelist=subgraph_nodes, node_color='red')
+    nx.draw_networkx_edges(graph, pos, edgelist=subgraph_edges, edge_color='red')
+    nx.draw_networkx_labels(graph, pos)
+    plt.show()
+
+def grakel_to_networkx(grakel_graph):
+    # Initialize a new NetworkX graph
+    networkx_graph = nx.Graph()
+
+    # Get the nodes and edges from the GraKeL graph
+    node_labels = grakel_graph.get_labels(purpose='dictionary', label_type='vertex')
+
+    # Add the nodes and their labels to the NetworkX graph
+    for node, label in node_labels.items():
+        networkx_graph.add_node(node, label=label)
+        print(label)
+
+    # Add the edges to the NetworkX graph
+    for ((node1, node2), weight) in grakel_graph.get_edges(purpose="dictionary"):
+        networkx_graph.add_edge(node1, node2)
+
+    return networkx_graph
+
+def get_substructure_labels(feature_vector):
+    feature_map = representation(graphs, 1)[1]
+    print(feature_map)
+    node_mappings = representation(graphs, 1)[0]
+    print((node_mappings))
+    inverse_node_mappings = {v: k for k, v in node_mappings.items()}
+    # Get indices of non-zero entries in the feature vector, increment each index by 22
+    positions = [index+22 for index, value in enumerate(feature_vector) if value > 0]
+    
+    # Translate positions to the corresponding keys in the feature map
+    keys = [k for k, v in feature_map.items() if v in positions]
+    
+    # Extract the node labels from the keys and translate them back to the original labels
+    substructure_labels = []
+    for key in keys:
+        # extract all numbers from the key and convert them to integers
+        node_indices = [int(s) for s in findall(r'\b\d+\b', key)]
+        labels = [inverse_node_mappings[i] for i in node_indices]
+        substructure_labels.append(labels)
+
+    return substructure_labels
+
+
 
 json_file = 'dictionary.json'
 classes = get_classes_from_json(json_file)
